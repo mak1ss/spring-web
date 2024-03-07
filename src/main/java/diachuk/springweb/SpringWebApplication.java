b27cc3e6dcad22f4d22b397d6303fb1daf92a151
Maksym Diachuk <dia4uk.maksim@gmail.com>
2024-03-07 22:46:51 +0200

    Used almost all the thymeleaf expressions

--- src/main/java/diachuk/springweb/SpringWebApplication.java
+++ src/main/java/diachuk/springweb/SpringWebApplication.java
@@ -3,7 +3,8 @@ package diachuk.springweb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+
+@SpringBootApplication()
 public class SpringWebApplication {
 
     public static void main(String[] args) {
--- src/main/java/diachuk/springweb/controllers/TestController.java
+++ src/main/java/diachuk/springweb/controllers/TestController.java
@@ -1,10 +1,12 @@
 package diachuk.springweb.controllers;
 
+import diachuk.springweb.entities.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.HashMap;
 import java.util.List;
 
 @Controller
@@ -15,6 +17,17 @@ public class TestController {
     public String getInfo(Model model){
         model.addAttribute("list", List.of("Hello", "Spring", "Boot"));
         model.addAttribute("string", "Made by Diachuk Maksym");
+        //       model.addAttribute("greeting", "Hello!");
+        HashMap<String, String> map  = new HashMap<>();
+
+        map.put("Kyiv", "The capital of Ukraine");
+        map.put("Vienna", "The capital of Hungary");
+
+        model.addAttribute("map", map);
+
+        model.addAttribute("user", new User("Andrew", 23));
+
+
         return "index";
     }
 }
